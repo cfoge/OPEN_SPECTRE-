820f16480d9a627c4e99144c601db96bd14fb0be
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2023-11-21 15:47:20 +1100

    osc update, now has simulated square and sin working

--- src/scripts/make_sinwave_table.py
+++ src/scripts/make_sinwave_table.py
@@ -1,18 +1,23 @@
 import numpy as np
 
-def generate_sine_table(bits=12, resolution=180):
+def generate_sine_table(bits=12, resolution=360):
     values = []
     for i in range(resolution + 1):
         # Calculate the sine value for each angle
         angle_rad = np.radians(i)
-        sine_value = int((2**(bits-1) - 1) * np.sin(angle_rad))
+        if i < 180:
+            sine_value = int((2**(bits-1) - 1) * np.sin(angle_rad) + ((2**(bits-1) - 1)))
+        else:
+            sine_value = int((2**(bits-1) - 1) * np.sin(angle_rad) - ((2**(bits-1) - 1)))
 
-        # Convert to 12-bit two's complement format
-        if sine_value < 0:
-            sine_value += 2**bits
+        # Convert to unsigned representation
+        sine_value = sine_value & ((1 << bits) - 1)
 
         # Convert to binary representation and append to the list
         values.append(format(sine_value, f'0{bits}b'))
+
+    # Repeat the sine wave for a full 360 degrees
+    values *= 2
 
     return values
 
@@ -20,7 +25,8 @@ def generate_sine_table(bits=12, resolution=180):
 sine_table = generate_sine_table(bits=12, resolution=180)
 
 # Print the generated values
-for i, value in enumerate(sine_table):
-    print('"' + f"{value}" + '",')
+# Print every 2nd value
+for i in range(0, len(sine_table), 2):
+    print('"' + f"{sine_table[i]}" + '",')
 
 # You can use this sine_table in your VHDL code
