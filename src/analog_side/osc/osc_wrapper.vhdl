ecebc73300009ed18503fe1403354cd5c9fd69a0
robertdjordanbitbucket <robertjo@blackmagicdesign.com>
2024-01-04 14:52:19 +1100

    osc wrapper fixed up syntax

--- src/analog_side/osc/osc_wrapper.vhdl
+++ src/analog_side/osc/osc_wrapper.vhdl
@@ -20,8 +20,8 @@ end osc_wrapper;
 
 architecture Behavioral of osc_wrapper is
 
-  signal sine_out_i, sin_1, sin_dev, sin_dev_d, sin_dev_d2, sin_dev_mix : std_logic_vector(11 downto 0);
-  freq_dev                                                              : std_logic_vector(9 downto 0);
+  signal sin_out_main, sin_out_i, sin_1, sin_dev, sin_dev_d, sin_dev_d2, sin_dev_mix : std_logic_vector(11 downto 0);
+  signal freq_dev                                                       : std_logic_vector(9 downto 0);
   signal square_i                                                       : std_logic := '0';
 
 begin
@@ -36,7 +36,7 @@ begin
       sync_sel   => sync_sel,
       sync_plus  => sync_plus,
       sync_minus => sync_minus,
-      sin_out    => sin_out,
+      sin_out    => sin_out_main,
       square_out => square_out
     );
 
@@ -49,7 +49,7 @@ begin
     sync_sel   => sync_sel,
     sync_plus  => sync_plus,
     sync_minus => sync_minus,
-    sin_out    => sin_out,
+    sin_out    => sin_dev,
     square_out => square_out
     );
 
@@ -59,8 +59,9 @@ begin
       sin_dev_d  <= sin_dev;
       sin_dev_d2 <= sin_dev_d;
     end if;
+  end process;
 
-    level_dev : AlphaBlend
+    level_dev : entity work.AlphaBlend
     port
     map (signal1 => (others => '0'),
     signal2 => sin_dev_d2,
@@ -70,7 +71,7 @@ begin
     Adder_12bit_NoOverflow_inst : entity work.Adder_12bit_NoOverflow
       port
       map (
-      A        => sin_out,
+      A        => sin_out_main,
       B        => sin_dev_mix,
       Sum      => sin_out_i,
       Overflow => open
